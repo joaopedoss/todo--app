fe8cb74ccbb74b25bd29a79fdba223523507da49
Joao Pedro Silveira Santos <116076838+joaopedoss@users.noreply.github.com>
2023-11-19 18:47:18 -0300

    modelo dark pronto

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+function alterarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+
+    if (tema) {
+        let novoTema
+        
+        if (tema === "light") {
+            novoTema = "dark"
+            button.innerHTML = `<img src="imagens/sun-icon.png" alt="sol icone">`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else {
+            novoTema = "light"
+            button.innerHTML = `<img src="imagens/moon-icon.png" alt="lua icone">`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema") 
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        if (tema === "dark") {
+            body.classList.add("dark")
+            button.innerHTML = `<img src="imagens/sun-icon.png" alt="sol icone">`
+        } else {
+            body.classList.add("Light")
+            button.innerHTML = `<img src="imagens/moon-icon.png" alt="lua icone">`
+        }
+    }
+}
+
+verificarTema()
