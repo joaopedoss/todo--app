e5e220af1bbaef6ff2e0026b042ce52f1d5bbd8d
Joao Pedro Silveira Santos <116076838+joaopedoss@users.noreply.github.com>
2023-11-19 14:18:05 -0300

    criaçao primeira view do projeto

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá,mundo!")
+    resposta.render('home')
 })
+
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
